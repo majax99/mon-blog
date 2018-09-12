5f904a87f6bcb6dce9cfd75a73dce483f02aca06
Pierre-Yves BLANC <p.blanc@neosoft.local>
2018-09-12 14:57:36 +0200

    ajout mon-blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+	posts = [
+	{
+		title: 'Mon premier post',
+		content: 'Ceci est le premier post'
+		loveIts: 0,
+		created_at: new Date()
+	},
+	{
+		title: 'Mon second post',
+		content: 'Ceci est le second post'
+		loveIts: 0,
+		created_at: new Date()
+	},
+	{
+		title: 'Mon troisième post',
+		content: 'Ceci est le troisième post'
+		loveIts: 0,
+		created_at: new Date()
+	}
+	]
+
+
+
 }
